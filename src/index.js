ddc2a6428d844e5783189a4352eaed66e95c138a
Aaron Contreras <aaron@redradix.com>
2017-04-28 01:47:45 +0200

    Accept plain objects as request body. :memo:

--- src/index.js
+++ src/index.js
@@ -19,15 +19,49 @@ async function buildResponse(response) {
   return [ error, resStatus, resBody, resText, resHeaders ]
 }
 
+/**
+ * A BodyInit, as defined by the Fetch standard.
+ * @see [Body Mixin]{@link https://fetch.spec.whatwg.org/#body-mixin}
+ * @typedef {Blob|BufferSource|FormData|URLSearchParams|USVString} BodyInit
+ */
+
+/**
+ * prepareBody - Prepare body to be consumable by both Request and window.fetch
+ * This function accepts either a supported body entity, in which case it is
+ * returned as is, or an object, which will be stringified and returned
+ *
+ * @param {BodyInit|object} body
+ * @return {BodyInit} Either body, or a stringified version of the body object
+ */
+function prepareBody(body) {
+  if (typeof body === 'string' || // USVString
+      body instanceof Blob || // Blob
+      body instanceof FormData || // FormData
+      body instanceof URLSearchParams || // URLSearchParams
+      // BufferSource
+      body instanceof ArrayBuffer || body instanceof Int8Array ||
+      body instanceof Uint8Array || body instanceof Uint8ClampedArray ||
+      body instanceof Int16Array || body instanceof Uint16Array ||
+      body instanceof Int32Array || body instanceof Uint32Array ||
+      body instanceof Float32Array || body instanceof Float64Array) {
+    return body
+  }
+  return JSON.stringify(body)
+}
+
 // REVIEW: Make this a factory accepting options to deep merge.
 const fetchNetworkAdapter = (url, method, { body, headers, credentials } = {}) => {
   let aborted = false
   const abortionMock = (cb) => (...args) => aborted ? void 0 : cb(...args)
 
-  const options = { method, body, headers, credentials }
+  const options = { method, headers, credentials }
 
-  if (method === 'GET' || method === 'HEAD' || && body) {
-    url = `${url}${url.includes('?') ? '&' : '?'}${stringify(body)}`
+  if (body) {
+    if (method === 'GET' || method === 'HEAD') {
+      url = `${url}${url.includes('?') ? '&' : '?'}${stringify(body)}`
+    } else {
+      options.body = prepareBody(body)
+    }
   }
 
   const request = new Request(url, options)
